8c1d29f0067bf5e6e07a7d4d01b9b40d105eea12
pyama86 <pyama@pepabo.com>
2019-10-30 16:28:13 +0900

    add subnet

--- test/mrb_ifaddr.rb
+++ test/mrb_ifaddr.rb
@@ -3,7 +3,7 @@
 ##
 
 assert("IFAddr.get") do
-  assert_equal("127.0.0.1", IFAddr.get("lo"))
+  assert_equal("127.0.0.1/255.0.0.0", IFAddr.get("lo"))
   assert_raise(RuntimeError) do
     IFAddr.get("notfound")
   end
